7b0fcd1e2a8e2d1132f5be35aca201f24f2de93e
tennessine <66058083+simonfalke-01@users.noreply.github.com>
2023-04-29 16:10:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -45,9 +45,7 @@ for (const file of files) {
 
 const start = async () => {
 	try {
-		await fastify.listen({
-			port: 3000,
-		});
+		await fastify.listen(port, '0.0.0.0');
 	}
 	catch (err) {
 		fastify.log.error(err);

37e8e27403e1d425b720ae839ca4c72356563cba
simonfalke-01 <ziqibrandonli@gmail.com>
2023-05-01 06:58:36 +0000

    changed webhook

--- webhooks/index.js
+++ webhooks/index.js
@@ -1,76 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
-const crypto = require('crypto');
 
 const app = express();
-const port = process.env.PORT || 3001;
-const secret = process.env.GITHUB_WEBHOOK_SECRET;
+const port = 3001;
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/webhook', (req, res) => {
-  const event = req.headers['x-github-event'];
-  const branch = req.body.ref.split('/').pop();
-  const signature = req.headers['x-hub-signature'];
-
-  const hmac = crypto.createHmac('sha1', secret);
-  const digest = Buffer.from('sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex'), 'utf8');
-  const checksum = Buffer.from(signature, 'utf8');
-
-  if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
-    console.log('Invalid signature');
-    res.status(400).send('Invalid signature');
-    return;
-  }
-
-  if (event === 'push' && branch === 'main') {
-    console.log('Received push event to main branch');
-    // Stop the server
-    exec('cd ../. && pm2 stop all', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error stopping server: ${error}`);
-        res.status(500).send('Error stopping server');
-      } else {
-        console.log(`Stopped server: ${stdout}`);
-        // Pull the latest code from GitHub
-        exec('cd ../. git pull', (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error pulling code from GitHub: ${error}`);
-            res.status(500).send('Error pulling code from GitHub');
-          } else {
-            console.log(`Pulled code from GitHub: ${stdout}`);
-            // Install dependencies
-            exec('cd ../. yarn install', (error, stdout, stderr) => {
-              if (error) {
-                console.error(`Error installing dependencies: ${error}`);
-                res.status(500).send('Error installing dependencies');
-              } else {
-                console.log(`Installed dependencies: ${stdout}`);
-                // Start the server
-                exec('cd ../. yarn start', (error, stdout, stderr) => {
-                  if (error) {
-                    console.error(`Error starting server: ${error}`);
-                    res.status(500).send('Error starting server');
-                  } else {
-                    console.log(`Started server: ${stdout}`);
-                    res.status(200).send('Server restarted');
-                  }
-                });
-              }
-            });
-          }
-        });
-      }
-    });
-  } else {
-    console.log(`Received unsupported event or branch: ${event} ${branch}`);
-    res.status(200).send('OK');
-  }
+app.post('/github-webhook', (req, res) => {
+  console.log('Received a webhook request!');
+  // Pull the latest changes from GitHub and restart the app
+  exec('cd ../. && git pull', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing git pull: ${error}`);
+      res.status(500).send('Error executing git pull');
+    } else {
+      console.log(`Git pull completed: ${stdout}`);
+      exec('cd ../. && yarn install && nohup pm2 restart api', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing yarn install: ${error}`);
+          res.status(500).send(`Error executing yarn install`);
+        } else {
+          res.status(200).send('OK');
+        }
+      });
+    }
+  });
 });
 
+// Start the Express.js app
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Webhook receiver listening at http://0.0.0.0:${port}`);
+  console.log(`Webhook server started on port ${port}`);
 });
-

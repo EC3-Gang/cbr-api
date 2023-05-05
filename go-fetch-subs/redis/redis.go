5203c1ad646666a5676c3d94e7a91c7044825496
simonfalke <ziqibrandonli@gmail.com>
2023-05-05 23:06:35 +0800

    stuff

--- go-fetch-subs/redis/redis.go
+++ go-fetch-subs/redis/redis.go
@@ -4,19 +4,36 @@ import (
 	"context"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	"log"
 )
 
 func test(client *redis.Client, ctx context.Context) {
 	err := client.Set(ctx, "foo", "bar", 0).Err()
 	if err != nil {
-		panic(err)
+		log.Println("[!] Failed to set foo: %w", err)
 	}
 
 	val, err := client.Get(ctx, "foo").Result()
 	if err != nil {
-		panic(err)
+		log.Println("[!] Failed to get foo: %w", err)
 	}
 	fmt.Println("foo", val)
+}
+
+func getAllData(client *redis.Client, ctx context.Context) {
+	// Get all keys and values
+	keys, err := client.Keys(ctx, "*").Result()
+	if err != nil {
+		log.Println("[!] Failed to get keys: %w", err)
+	}
+
+	for _, key := range keys {
+		val, err := client.Get(ctx, key).Result()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(key, val)
+	}
 }
 
 func main() {
@@ -28,5 +45,5 @@ func main() {
 
 	ctx := context.Background()
 
-	test(client, ctx)
+	getAllData(client, ctx)
 }

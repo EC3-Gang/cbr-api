6e019beec74e1e488f41a82e4a6063853c44ed3c
simonfalke <ziqibrandonli@gmail.com>
2023-05-05 22:55:39 +0800

    redis

--- go-fetch-subs/redis/redis.go
+++ go-fetch-subs/redis/redis.go
@@ -6,15 +6,7 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
-
-	ctx := context.Background()
-
+func test(client *redis.Client, ctx context.Context) {
 	err := client.Set(ctx, "foo", "bar", 0).Err()
 	if err != nil {
 		panic(err)
@@ -25,4 +17,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("foo", val)
+}
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	ctx := context.Background()
+
+	test(client, ctx)
 }

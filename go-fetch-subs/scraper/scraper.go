59c42a4f6553c1a40c30e67b46fef1b1951aab9d
simonfalke <ziqibrandonli@gmail.com>
2023-05-06 11:18:49 +0800

    bruh

--- go-fetch-subs/scraper/scraper.go
+++ go-fetch-subs/scraper/scraper.go
@@ -114,15 +114,83 @@ func GetSinglePageAttempts(page int, problemID string) *[]types.Attempt {
 }
 
 func GetPageAttempts(page int, problemID string, currentAttempts *[]types.Attempt, wg *sync.WaitGroup) {
-	url := formatCBRUrl(page, problemID)
-	doc, err := getUrl(url)
+	url := fmt.Sprintf("https://codebreaker.xyz/submissions?problem=%s&page=%d", problemID, page)
+	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		log.Printf("[!] Failed to get page attempts: %v", err)
+		//errChan <- fmt.Errorf("failed to get page attempts: %w", err)
 		return
 	}
 
-	attempts := parseAttempts(doc)
-	*currentAttempts = append(*currentAttempts, attempts...)
+	// Check if there are no attempts on this page
+	//if doc.Find(".table tbody tr").Length() == 0 {
+	//	//retChan <- []Attempt{}
+	//}
+
+	//var attempts []Attempt
+
+	doc.Find(".table tbody tr").Each(func(i int, s *goquery.Selection) {
+		attempt := types.Attempt{}
+		s.Find("td").Each(func(j int, ss *goquery.Selection) {
+			switch j {
+			case 0:
+				idStr := ss.Text()
+				idStr = processString(idStr)
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					log.Printf("failed to parse ID on attempt %d: %v", i, err)
+				}
+				attempt.ID = id
+			case 1:
+				submissionStr := ss.Text()
+				submission, err := time.Parse("2006-01-02 15:04:05", submissionStr)
+				if err != nil {
+					log.Printf("failed to parse submission time on attempt %d: %v", i, err)
+				}
+				attempt.Submission = submission
+			case 2:
+				attempt.Username = processString(ss.Text())
+			case 3:
+				attempt.Problem = processString(ss.Text())
+			case 4:
+				scoreStr := ss.Text()
+				scoreStr = processString(scoreStr)
+				score, err := strconv.ParseFloat(scoreStr, 64)
+				if err != nil {
+					log.Printf("failed to parse score on attempt %d: %v", i, err)
+				}
+				attempt.Score = score
+			case 5:
+				attempt.Language = processString(ss.Text())
+			case 6:
+				maxTimeStr := ss.Text()
+				maxTimeStr = processString(maxTimeStr)
+				maxTime := 0.0
+				if maxTimeStr != "N/A" {
+					maxTime, err = strconv.ParseFloat(maxTimeStr, 64)
+				} else {
+					maxTime = -1.0
+				}
+				if err != nil {
+					log.Printf("failed to parse max time on attempt %d: %v", i, err)
+				}
+				attempt.MaxTime = maxTime
+			case 7:
+				maxMemoryStr := ss.Text()
+				maxMemoryStr = processString(maxMemoryStr)
+				maxMemory := 0.0
+				if maxMemoryStr != "N/A" {
+					maxMemory, err = strconv.ParseFloat(maxMemoryStr, 64)
+				} else {
+					maxMemory = -1.0
+				}
+				if err != nil {
+					log.Printf("failed to parse max memory on attempt %d: %v", i, err)
+				}
+				attempt.MaxMemory = maxMemory
+			}
+		})
+		*currentAttempts = append(*currentAttempts, attempt)
+	})
 	wg.Done()
 }
 
